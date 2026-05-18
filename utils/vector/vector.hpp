c897e2a76c305bae99c30d9c9a287e8b06035e01
ba1chev <baychevyoan@gmail.com>
2026-05-18 13:42:46 +0300

    Implemented vector util

---
+++ utils/vector/vector.hpp
@@ -0,0 +1,204 @@
+#pragma once
+
+#include <iostream>
+
+template <class T>
+class Vector {
+private:
+    T* data = nullptr;
+    size_t size = 0;
+    size_t capacity = 0;
+
+    void free();
+    void resize(size_t newCapacity);
+    void copyFrom(const Vector& other);
+    void moveTo(Vector&& other) noexcept;
+    size_t calculateNeededCapacity(size_t size) const;
+    
+public:
+    Vector();
+    Vector(const T* data, size_t dataSize);
+    Vector(size_t capacity);
+    Vector(const Vector& other);
+    Vector(Vector&& other) noexcept;
+    Vector& operator = (const Vector& other);
+    Vector& operator = (Vector&& other) noexcept;
+    ~Vector();
+
+    void pop_back();
+    void push_back(const T& data);
+    void push_back(T&& data);
+
+    const T& operator [] (size_t index) const;
+    T& operator [] (size_t index);
+
+    size_t getSize() const;
+    size_t getCapaciy() const;
+    const T* getData() const;
+
+    template <class U>
+    friend std::ostream& operator << (std::ostream& os, const Vector<U>& vector);
+
+    template <class U>
+    friend std::istream& operator >> (std::istream& is, Vector<U>& vector);
+};
+
+template <class T>
+Vector<T>::Vector() {
+    this->capacity = 8;
+    this->size = 0;
+    this->data = new T[this->capacity] {};
+}
+
+template <class T>
+Vector<T>::Vector(const T* data, size_t dataSize) {
+    if (!data) {
+        throw std::runtime_error("Nullptr detected");
+    }
+
+    this->size = dataSize;
+    this->capacity = calculateNeededCapacity(this->size);
+    this->data = new T[this->capacity] {};
+
+    for (size_t i = 0; i < this->size; i++) {
+        this->data[i] = data[i];
+    }
+}
+
+template <class T>
+Vector<T>::Vector(size_t capacity) {
+    this->size = 0;
+    this->capacity = capacity;
+    this->data = new T[this->capacity] {};
+}
+
+template <class T>
+Vector<T>::Vector(const Vector& other) {
+    this->copyFrom(other);
+}
+
+template <class T>
+Vector<T>::Vector(Vector&& other) noexcept {
+    this->moveTo(std::move(other));
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator = (const Vector& other) {
+    if (this != &other) {
+        this->free();
+        this->copyFrom(other);
+    }
+
+    return *this;
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator = (Vector&& other) noexcept {
+    if (this != &other) {
+        this->free();
+        this->moveTo(std::move(other));
+    }
+
+    return *this;
+}
+
+template <class T>
+Vector<T>::~Vector() {
+    this->free();
+}
+
+
+template <class T>
+void Vector<T>::pop_back() {
+    if (this->size == 0) {
+        throw std::runtime_error("The vector is already empty");
+    }
+
+    this->data[this->size - 1] = T{};
+    this->size -= 1;
+}
+
+template <class T>
+void Vector<T>::push_back(const T& data) {
+    if (this->size == this->capacity) {
+        this->resize(this->calculateNeededCapacity(this->size));
+    }
+
+    this->data[this->size] = data;
+    this->size += 1;
+}
+
+template <class T>
+void Vector<T>::push_back(T&& data) {
+    if (this->size == this->capacity) {
+        this->resize(this->calculateNeededCapacity(this->size));
+    }
+
+    this->data[this->size] = std::move(data);
+    this->size += 1;
+}
+
+template <class T>
+size_t Vector<T>::getSize() const {
+    return this->size;
+}
+
+template <class T>
+size_t Vector<T>::getCapaciy() const {
+    return this->capacity;
+}
+
+template <class T>
+const T* Vector<T>::getData() const {
+    return this->data;
+}
+
+template <class T>
+const T& Vector<T>::operator [] (size_t index) const {
+    if (index >= this->size) {
+        throw std::out_of_range("Index is out of range");
+    }
+
+    return this->data[index];
+}
+
+template <class T>
+T& Vector<T>::operator [] (size_t index) {
+    if (index >= this->size) {
+        throw std::out_of_range("Index is out of range");
+    }
+    
+    return this->data[index];
+}
+
+template <class T>
+std::ostream& operator << (std::ostream& os, const Vector<T>& vector) {
+    os << vector.getSize() << ", ";
+
+    for (size_t i = 0; i < vector.getSize(); i++) {
+        if (i != vector.getSize() - 1) {
+            os << vector[i] << ", ";
+        } else {
+            os << vector[i];
+        }
+    }
+
+    return os;
+}
+
+template <class T>
+std::istream& operator >> (std::istream& is, Vector<T>& vector) {
+    size_t size = 0;
+    T currentElement {};
+
+    is >> size;
+    Vector<T> newVector(vector.calculateNeededCapacity(size));
+
+    for (size_t i = 0; i < size; i++) {
+        is >> currentElement;
+        newVector.push_back(currentElement);
+    }
+    
+    vector = newVector;
+    return is;
+}
