d7d47c22a30228a4a4029f9b0f2d322dedc75e73
ba1chev <baychevyoan@gmail.com>
2026-07-29 17:20:47 +0300

    Assembled the kmeans validator (interface)

---
+++ source/learning_models/input_validation/clustering_validation/kmeans_validator/kmeans_validator.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "source/learning_models/input_validation/input_validator.h"
+
+class KMeansValidator: public InputValidator {
+public:
+    bool validate(int argc, const char* argv[]) const override;
+    InputValidatorType get_type() const override;
+};
