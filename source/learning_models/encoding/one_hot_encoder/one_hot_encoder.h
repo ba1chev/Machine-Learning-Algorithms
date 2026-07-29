c484f04dbed5cb5a65935d3ec2dbbabe2aae884a
ba1chev <baychevyoan@gmail.com>
2026-07-29 17:20:46 +0300

    Launched the one hot encoder (interface)

---
+++ source/learning_models/encoding/one_hot_encoder/one_hot_encoder.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <memory>
+#include <string>
+
+#include "source/learning_models/encoding/encoder_template.hpp"
+
+class OneHotEncoder: public EncoderTemplate<Matrix<std::string>, Matrix<float>> {
+public:
+    Matrix<float> encode(const Matrix<std::string>& data) const override;
+    std::unique_ptr<EncoderTemplate<Matrix<std::string>, Matrix<float>>> clone() const override;
+    EncoderType get_type() const override;
+};
